bd7e0e0284f8ceffa531d2dedb4a664759158189
David Hunt <davhunt@indiana.edu>
2019-04-01 17:23:12 -0400

    round bvals

---
+++ round_bvals.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+import shutil
+import sys
+import os
+
+with open(sys.argv[1]) as f:
+    bvals = f.read().split(' ')
+    bvals = [x for x in bvals if x != '']
+    bvals = [x for x in bvals if x != '\n']
+
+with open('dwi.bvals','w') as f:
+    for bval in bvals:
+	bval = round(float(bval),-2)
+        f.write("%s " %bval)
+
+shutil.move(os.path.join(os.getcwd(),'dwi.bvals'), sys.argv[1])
