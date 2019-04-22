628748af2bde02d8de5955ad871ea0080c7959a2
David Hunt <davhunt@indiana.edu>
2019-04-22 12:13:07 -0400

    exit if no b0 images

--- round_bvals.py
+++ round_bvals.py
@@ -3,6 +3,7 @@
 import shutil
 import sys
 import os
+import subprocess
 
 with open(sys.argv[1]) as f:
     bvals = f.read().split(' ')
@@ -10,10 +11,17 @@ with open(sys.argv[1]) as f:
     bvals = [x for x in bvals if x != '\n']
     bvals = [x for x in bvals if x != ' ']
 
+b0_present = False
 with open('dwi.bvals','w') as f:
     for bval in bvals:
         bval = bval.replace('\n','')
 	bval = round(float(bval),-2)
+        if bval == 0:
+            b0_present = True
         f.write("%s " %bval)
+
+#exit if there are no b0 images
+if b0_present == False:
+    sys.exit("At least one b0 image must be present in the DWI")
 
 shutil.move(os.path.join(os.getcwd(),'dwi.bvals'), sys.argv[1])
